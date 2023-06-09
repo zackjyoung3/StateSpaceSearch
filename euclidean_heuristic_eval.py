ac6d5b968a891639452caa1fc88316d2374e3d54
zackjyoung3 <zjy@georgetown.edu>
2023-06-09 12:18:29 -0400

    Changed the name for city_node to city because I thought that was more proper as calling it a city node may make the implication that the city is a node rather than a state in a state space search problem

--- euclidean_heuristic_eval.py
+++ euclidean_heuristic_eval.py
@@ -1,13 +1,13 @@
 from evaluation_function import HeuristicEvaluationFunction
-from city_node import CityNode
+from city import City
 from constant_cities_graph import euclidean_distance
 
 
 class Euclidean_Distance(HeuristicEvaluationFunction):
-    def __init__(self, goal_state: CityNode = None):
+    def __init__(self, goal_state: City = None):
         super().__init__()
 
-    def evaluate(self, city: CityNode, cost: float):
+    def evaluate(self, city: City, cost: float):
         if self.goal_state is None:
             raise RuntimeError("Did not set the goal state for the heuristic evaluation function")
         else:
